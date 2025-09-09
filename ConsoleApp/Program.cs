6ba933ae7d029c42fe1a6a9a10b1178348d7ffb5
Muhammad Naufal Farras <opal.farras@gmail.com>
2025-09-09 14:12:05 +0700

    Create Logic Exercise

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Panda p1 = new("Pan Dee");
+Panda p2 = new("Pan Dah");
+
+Console.WriteLine(p1.Name);
+Console.WriteLine(p2.Name);
+Console.WriteLine(Panda.Population);
+
+public class Panda
+{
+    public string Name;
+    public static int Population;
+
+    public Panda(string n)
+    {
+        Name = n;
+        Population += 1;
+    }
+}
---
+++ LogicExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿static void FooBar(int n)
+{
+    for (int i = 1; i < n + 1; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            Console.Write("foobar" + LastCharCheck(i, n));
+        else if (i % 3 == 0)
+            Console.Write("foo" + LastCharCheck(i, n));
+        else if (i % 5 == 0)
+            Console.Write("bar" + LastCharCheck(i, n));
+        else
+            Console.Write(i + LastCharCheck(i, n));
+    }
+}
+
+static string LastCharCheck(int target, int length)
+{
+    return target != length ? ", " : "";
+}
+
+FooBar(15);
