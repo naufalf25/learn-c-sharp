d57d83514968b90d4c7866dd34b8b9b23a6ba19a
Muhammad Naufal Farras <opal.farras@gmail.com>
2025-09-11 11:11:32 +0700

    Create new Classes and Inheritance program

---
+++ Classes/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Text.Json;
+
+User test = new User("Naufal");
+Console.WriteLine(test.DisplayName());
+test.DisplayRange(20);
+test.DisplayRange(50.5);
+
+User test2 = new User("Farras", 24);
+Console.WriteLine(test2.DisplayName());
+Console.WriteLine(test2.DisplayAge());
+
+
+Rectangle rect = new Rectangle(20, 10);
+(float width, float height) = rect;
+Console.WriteLine($"The rectangle has width {width} and height {height}");
+
+
+Bunny b1 = new Bunny { Name = "Bo", LikesCarrots = true, LikesHumans = false };
+Bunny b2 = new Bunny("Yo") { LikesCarrots = true, LikesHumans = false };
+Console.WriteLine(b1.Name);
+Console.WriteLine(b2.Name);
+
+
+Property property = new Property("Naufal");
+property.propertyCount = 10;
+property.DisplayOwnProperty();
+
+
+public class User
+{
+    private readonly string _name;
+    private readonly int _age;
+
+    public User(string n)
+    {
+        this._name = n;
+    }
+
+    public User(string name, int age) : this(name) => _age = age;
+
+    public string DisplayName()
+    {
+        return _name;
+    }
+
+    public int DisplayAge()
+    {
+        return _age;
+    }
+
+    public void DisplayRange(int range)
+    {
+        Console.WriteLine($"The range in {range.GetType().Name} is {range}");
+    }
+
+    public void DisplayRange(double range)
+    {
+        Console.WriteLine($"The range in {range.GetType().Name} is {range}");
+    }
+}
+
+public class Rectangle
+{
+    private readonly float _width;
+    private readonly float _height;
+
+    public Rectangle(float width, float height)
+    {
+        this._width = width;
+        this._height = height;
+    }
+
+    public void Deconstruct(out float width, out float height)
+    {
+        width = this._width;
+        height = this._height;
+    }
+}
+
+public class Bunny
+{
+    public string Name;
+    public bool LikesCarrots;
+    public bool LikesHumans;
+
+    public Bunny() { }
+    public Bunny(string n) => Name = n;
+}
+
+public class Property
+{
+    private string _ownerName;
+    public int propertyCount { get; set; }
+
+    public Property(string name)
+    {
+        this._ownerName = name;
+    }
+
+    public void DisplayOwnProperty()
+    {
+        var obj = new { name = _ownerName, propertyCount };
+        Console.WriteLine("The results of Property:");
+
+        // Console.WriteLine($@"{{""name"":""{obj.name}"",""propertyCount""{obj.propertyCount}}}"); 
+        // Displaying object not effective using this method. Better using JSON format using package
+
+        string json = JsonSerializer.Serialize(obj); // Better result
+        Console.WriteLine(json);
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,154 @@
+﻿Student student = new Student { Name = "Fer", Email = "test@test.com", Age = 18, StudentId = 12345, Grade = 12, GPA = 3.68 };
+student.DisplayUserDetail();
+student.DisplayStudentInfo();
+Console.WriteLine();
+
+Employee employee = new Employee { Name = "Jun", Email = "test2@test.com", Age = 22, EmployeeId = 123354, Year = 2 };
+employee.DisplayUserDetail();
+employee.DisplayEmployeeInfo();
+Console.WriteLine();
+
+Display(student);
+// Display(new User()); // Will be got error
+Console.WriteLine();
+
+static void Display(Student student)
+{
+    Console.WriteLine(student.Name);
+}
+
+
+// Upcasting
+Console.WriteLine("Upcasting");
+User a = student;
+Console.WriteLine(a.Name);
+Console.WriteLine();
+
+// Downcasting
+Console.WriteLine("Downcasting");
+User b = student;
+Student s = (Student)b;
+// Student s2 = (Employee)b; // Will be got error
+Console.WriteLine(s.StudentId);
+Console.WriteLine(s == a);
+Console.WriteLine(s == student);
+Console.WriteLine();
+
+
+User c = new User();
+// as Operator
+// Student s3 = c as Student; // Will be null
+
+// is Operator
+if (c is Student student1)
+    Console.WriteLine(student1.StudentId);
+else
+    Console.WriteLine(false);
+Console.WriteLine();
+
+
+// Virtual Function Members
+Console.WriteLine("---Virtual Function Members---");
+House mansion = new House { Name = "McMansion", Mortgage = 2500000 };
+Asset d = mansion;
+Console.Write(mansion.Liability);
+Console.WriteLine(" // House's overridden implementation");
+Console.Write(d.Liability);
+Console.WriteLine(" // Polymorphism: Calls House's implementation");
+Console.WriteLine();
+
+// Abstack Function Members
+Console.WriteLine("---Abstract Function Members---");
+Stock ownStock = new Stock { SharesOwner = 25000, CurrentPrice = 500000000 };
+Console.Write(ownStock.NetValue);
+Console.WriteLine(" // Must provide implementation from abstract Asset function");
+Console.WriteLine();
+
+// Hiding Inherited Members
+Console.WriteLine("---Hiding Inherited Members---");
+A newA = new B();
+Console.WriteLine(newA.Counter);
+B newB = new B();
+Console.WriteLine(newB.Counter);
+Console.WriteLine();
+
+
+public class User
+{
+    public string Name = "";
+    public string Email = "";
+    public int Age;
+
+    public void DisplayUserDetail()
+    {
+        Console.WriteLine("The detail of this user:");
+        Console.WriteLine($"Name: {Name}, Email: {Email}, Age: {Age}");
+    }
+}
+
+public class Student : User
+{
+    public int StudentId;
+    public int Grade;
+    public double GPA;
+
+    public void DisplayStudentInfo()
+    {
+        Console.WriteLine("This user has student information:");
+        Console.WriteLine($"ID: {StudentId}, Grade: {Grade}, GPA: {GPA}");
+    }
+}
+
+public class Employee : User
+{
+    public int EmployeeId;
+    public int Year;
+
+    public void DisplayEmployeeInfo()
+    {
+        Console.WriteLine("This user has Employee Information:");
+        Console.WriteLine($"ID: {EmployeeId}, Year in this company: {Year}");
+    }
+}
+
+public abstract class Asset
+{
+    public string Name = "";
+    public virtual decimal Liability => 0;
+    // public virtual Asset Clone() => new Asset { Name = Name };
+    public abstract decimal NetValue { get; }
+}
+
+public class Stock : Asset
+{
+    public long SharesOwner;
+    public decimal CurrentPrice;
+    public override decimal NetValue => CurrentPrice * SharesOwner;
+}
+
+public class House : Asset
+{
+    public decimal Mortgage;
+    public override decimal Liability => base.Liability + Mortgage; // base keyword implementation
+
+    public override decimal NetValue => throw new NotImplementedException();
+
+    // public override Asset Clone() => new House { Name = Name, Mortgage = Mortgage };
+}
+
+public class A { public int Counter = 1; }
+public class B : A { public new int Counter = 2; }
+
+public class Baseclass
+{
+    public int X;
+    public Baseclass() { }
+    public Baseclass(int x) => X = X;
+}
+
+// public class Subclass : Baseclass { } // ILLEGAL: Subclass doesn't have a constructor taking an int
+
+public class Subclass : Baseclass
+{
+    public Subclass(int x) : base(x) {}
+}
