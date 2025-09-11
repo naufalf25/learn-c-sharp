5a185b4090430857856155b6e5c3f951a708eb1a
Muhammad Naufal Farras <opal.farras@gmail.com>
2025-09-11 11:47:17 +0700

    Create ObjectType and Struct Program

---
+++ ObjectType/Program.cs
@@ -0,0 +1,38 @@
+﻿// Object Type
+Console.WriteLine("---Why Object Type Useful?---");
+Stack stack = new Stack();
+stack.Push("sausage");
+string s = (string)stack.Pop();
+Console.WriteLine(s);
+
+stack.Push(3);
+int three = (int)stack.Pop();
+Console.WriteLine(three);
+
+stack.Push(5.5);
+double gotDouble = (double)stack.Pop();
+Console.WriteLine(gotDouble);
+Console.WriteLine();
+
+Console.WriteLine("---Boxing and Unboxing---");
+// Boxing
+int x = 20;
+object obj = x;
+Console.Write(obj);
+Console.WriteLine(" // Boxing implementation");
+
+// Unboxing
+int y = (int)obj;
+Console.Write(y);
+Console.WriteLine(" // Unboxing implementation");
+Console.WriteLine();
+
+
+public class Stack
+{
+    int position;
+    readonly object[] data = new object[10];
+
+    public void Push(object obj) { data[position++] = obj; }
+    public object Pop() { return data[--position]; }
+}
---
+++ Struct/Program.cs
@@ -0,0 +1,17 @@
+﻿// Default Contructor
+Point p1 = new Point();
+Console.WriteLine(p1.y);
+
+Point p2 = default;
+Console.WriteLine(p2.x);
+Console.WriteLine(p2.y);
+
+// p2.x = 10; // should be error because read only
+
+readonly struct Point
+{
+    public readonly int x = 1;
+    public readonly int y;
+
+    public Point() => y = 1;
+}
