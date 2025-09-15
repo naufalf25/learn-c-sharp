131f1cb10b55d7a17c2d16bf434842692fa0bb36
Muhammad Naufal Farras <opal.farras@gmail.com>
2025-09-15 09:26:35 +0700

    Update foldering program by category

--- Projects/LogicExercise/Program.cs
+++ Projects/LogicExercise/Program.cs
@@ -12,9 +12,20 @@
             results.Add("bar");
         else
             results.Add(i);
+
+        if (i == n && i % 7 == 0)
+            results[i - 1] += "jazz";
     }
 
     Console.WriteLine(string.Join(", ", results));
 }
 
+Console.WriteLine("---Logic Exercise---");
+Console.WriteLine("\nFor using parameter 15:");
 FooBar(15);
+Console.WriteLine("\nFor using parameter 21:");
+FooBar(21);
+Console.WriteLine("\nFor using parameter 35:");
+FooBar(35);
+Console.WriteLine("\nFor using parameter 105:");
+FooBar(105);
