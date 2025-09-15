89a15b5b8eef955f55fa97d85bd299930c643958
Muhammad Naufal Farras <opal.farras@gmail.com>
2025-09-15 13:03:14 +0700

    Create new Event and TryException program

---
+++ Practice/Advanced/TryException/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exception = System.Exception;
+
+// Try Statement and Exception
+Console.WriteLine
+("\n============Try Statement and Exception===========");
+
+// Example: Handling Division by Zero
+Console.WriteLine("---Example: Handling Division by Zero---");
+int Calc(int x) => 10 / x;
+
+try
+{
+    int y = Calc(0);
+    Console.WriteLine(y);
+}
+catch (DividedByZeroException ex)
+{
+    Console.WriteLine("Error: x cannot be zero.");
+}
+Console.WriteLine("Program completed.");
