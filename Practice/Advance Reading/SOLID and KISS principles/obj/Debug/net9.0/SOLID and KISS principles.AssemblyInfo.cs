c325b6857f6fd92ea4b4239a823f0d45f9a81390
Muhammad Naufal Farras <opal.farras@gmail.com>
2025-10-08 10:17:00 +0700

    feat: add .gitignore

--- Practice/Advance Reading/SOLID and KISS principles/obj/Debug/net9.0/SOLID and KISS principles.AssemblyInfo.cs
+++ Practice/Advance Reading/SOLID and KISS principles/obj/Debug/net9.0/SOLID and KISS principles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SOLID and KISS principles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3aba489e67eb4ec121351538c2b5e86065a2da4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24582cb5994345d4a5232277a2777b26a42de3a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SOLID and KISS principles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SOLID and KISS principles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Projects/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ Projects/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+004a0467c2cc42926fcf9e575937b3ad8dde0552")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3aba489e67eb4ec121351538c2b5e86065a2da4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
